f8b5faa9437d07803c61f80ca2a4322f8d6d0641
Jordan Bice <jmbice@gmail.com>
2019-03-13 15:21:49 -0700

    Render new root component with app. Link React, SCSS and images

--- client/app.jsx
+++ client/app.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/Root';
+import './styles/main.scss';
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('IGN-webApp-FE'));
+ReactDOM.render(<Root />, document.getElementById('IGN-webApp-FE'));
