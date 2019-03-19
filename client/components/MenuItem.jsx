a8ade96285bfb13aaab7a8636b78412b08ec4fcc
Jordan Bice <jmbice@gmail.com>
2019-03-18 18:13:24 -0700

    Create menu component ele to display correct PNG and css based on state

---
+++ client/components/MenuItem.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const MenuItem = (props) => {
+  const { itemType, text, changeFilter, filter, passive, active, selected } = props;
+
+  let item = passive;
+  let latestHover = active;
+  if (filter === itemType) {
+    item = selected;
+    latestHover = selected;
+  }
+
+  return (
+    <button
+      type="button"
+      value={itemType}
+      className={filter === itemType ? 'menu-btn-active' : 'menu-btn'}
+      onClick={changeFilter}
+      onMouseOver={e => e.currentTarget.children[0].src = latestHover}
+      onMouseLeave={e => e.currentTarget.children[0].src = item}
+    >
+      <img className="menuPNG" src={item} alt="test" />
+      <div className="menuText"> {text} </div>
+    </button>
+  );
+};
+
+export default MenuItem;
