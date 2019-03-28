4aec37805136819c9d657d2d03d046d6bccbfc9d
Jordan Bice <jmbice@gmail.com>
2019-03-27 20:29:02 -0700

    Remove png images from props to children, and stop importing them

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -1,14 +1,4 @@
 import React from 'react';
-import latestPassive from '../../public/images/latest_passive.png';
-import latestActive from '../../public/images/latest_active.png';
-import latestSelected from '../../public/images/latest_selected.png';
-import videosPassive from '../../public/images/videos_passive.png';
-import videosActive from '../../public/images/videos_active.png';
-import videosSelected from '../../public/images/videos_selected.png';
-import articlesPassive from '../../public/images/articles_passive.png';
-import articlesActive from '../../public/images/articles_active.png';
-import articlesSelected from '../../public/images/articles_selected.png';
-
 import MenuItem from './MenuItem';
 
 
@@ -22,27 +12,18 @@ const Menu = (props) => {
         changeFilter={changeFilter}
         itemType="latest"
         text="Latest"
-        passive={latestPassive}
-        active={latestActive}
-        selected={latestSelected}
       />
       <MenuItem
         filter={filter}
         changeFilter={changeFilter}
         itemType="videos"
         text="Videos"
-        passive={videosPassive}
-        active={videosActive}
-        selected={videosSelected}
       />
       <MenuItem
         filter={filter}
         changeFilter={changeFilter}
         itemType="articles"
-        text="Articles"
-        passive={articlesPassive}
-        active={articlesActive}
-        selected={articlesSelected}
+        text="Articles"d}
       />
     </div>
   );
