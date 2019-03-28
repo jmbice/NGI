2e38644ce347ceb1ae95bc8fc751f48f148244b5
Jordan Bice <jmbice@gmail.com>
2019-03-27 20:19:40 -0700

    Remove anti-pattern of DOM manpulation, add className based on menu val

--- client/components/MenuItem.jsx
+++ client/components/MenuItem.jsx
@@ -3,23 +3,14 @@ import React from 'react';
 const MenuItem = (props) => {
   const { itemType, text, changeFilter, filter, passive, active, selected } = props;
 
-  let item = passive;
-  let latestHover = active;
-  if (filter === itemType) {
-    item = selected;
-    latestHover = selected;
-  }
-
   return (
     <button
       type="button"
       value={itemType}
       className={filter === itemType ? 'menu-btn-active' : 'menu-btn'}
       onClick={changeFilter}
-      onMouseOver={e => e.currentTarget.children[0].src = latestHover}
-      onMouseLeave={e => e.currentTarget.children[0].src = item}
     >
-      <img className="menuPNG" src={item} alt="latest" />
+      <div className={`png-${itemType}`} alt="latest" />
       <div className="menuText">{text}</div>
     </button>
   );
