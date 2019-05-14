65c171b731ecb8d912945c912adc77d1bc695088
Jordan Bice <jmbice@gmail.com>
2019-05-14 16:22:28 -0700

    Add production environement option for port

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const request = require('request');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 // paths, parseJSON = true, pars URL encoded = true, allow x-origin requests
